098089078962b6a069278dfa011290b0da9d5592
ruchiraw <ruchira.wageesha@gmail.com>
2014-07-20 08:29:54 +0530

    adding websocket support

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var fs = require('fs');
+var https = require('https');
+var uuid = require('node-uuid');
+var ws = require('ws');
 var build = require('build');
 var express = require('express');
 var mongoose = require('mongoose');
 var auth = require('auth');
 
+var str = JSON.stringify;
+var pas = JSON.parse;
+
 var mongourl = 'mongodb://localhost/hub';
 var HTTP_PORT = 4000;
 var app = express();
+
+var options = {
+    key: fs.readFileSync('ssl/hub.key'),
+    cert: fs.readFileSync('ssl/hub.cert')
+};
+
+var server = https.createServer(options, app);
 
 auth = auth({
     open: [
@@ -41,11 +54,38 @@ db.once('open', function callback() {
     app.use('/apis/v', require('./lib/menu'));
     //drones apis
     app.use('/apis/v', require('./lib/drone'));
-        //domains apis
+    //domains apis
     app.use('/apis/v', require('./lib/domain'));
 
+    var wss = new ws.Server({
+        server: server
+    });
+
+    var clients = [];
+    wss.on('connection', function (socket) {
+        console.log('connected');
+        clients.push(socket);
+        socket.on('message', function (o) {
+            o = pas(o);
+            console.log(o.data.data);
+        });
+        socket.on('disconnect', function () {
+            console.log('disconnected');
+        });
+    });
+
+    app.get('/wss', function (req, res) {
+        console.log(req.query.data);
+        var data = pas(req.query.data);
+        data.id = uuid.v4();
+        clients.forEach(function (client) {
+            client.send(str(data));
+        });
+        res.send('done');
+    });
+
     //hot component building
-    app.use(build);
+    //app.use(build);
 
     //index page
     app.all('*', function (req, res) {
@@ -53,6 +93,6 @@ db.once('open', function callback() {
         res.set('Content-Type', 'text/html').send(200, index);
     });
 
-    app.listen(HTTP_PORT);
+    server.listen(HTTP_PORT);
     console.log('listening on port ' + HTTP_PORT);
 });
