42eafb012f5b59f0cb04f1b1d43379ed588fbc5f
ruchiraw <ruchira.wageesha@gmail.com>
2014-08-01 00:01:32 +0530

    adding socproc

--- plugins/sh.js
+++ plugins/sh.js
@@ -1,38 +1,5 @@
-var uuid = require('node-uuid');
-var sockt = require('../lib/socket');
+var sessions = {};
 
-var PLUGIN = 'sh';
+module.exports.run = function (command, args, sid, fn) {
 
-var ctxs = {};
-
-module.exports = function (options, notify) {
-    console.log(options);
-    var server, id, ctx,
-        action = options.action;
-    switch (action) {
-        case 'run':
-            //TODO here
-            id = uuid.v4();
-            server = sockt.servers(options.id);
-            ctxs[id] = {
-                id: options.id,
-                server: server,
-                notify: notify
-            };
-            options.id = id;
-            server.socket.emit('do', options);
-            break;
-        case 'stdout':
-            console.log('-----------------------------sh---------------------------');
-            console.log(options.data);
-            break;
-        case 'stderr':
-            break;
-        case 'ran':
-            ctx = ctxs[options.id];
-            delete ctxs[options.id];
-            options.id = ctx.id;
-            ctx.notify(options);
-            break;
-    }
 };
---
+++ server.js
@@ -0,0 +1,52 @@
+var https = require('https');
+var express = require('express');
+var io = require('socket.io');
+var fs = require('fs');
+var constants = require('constants');
+var socproc = require('socproc');
+
+var HTTP_PORT = 4000;
+var app = express();
+
+var options = {
+    key: fs.readFileSync('ssl/hub.key'),
+    cert: fs.readFileSync('ssl/hub.cert')
+};
+
+var server = https.createServer(options, app);
+
+io = io(server);
+
+server.listen(HTTP_PORT);
+console.log('listening on port ' + HTTP_PORT);
+
+var sps = socproc('server', io);
+sps.on('connection', function (client) {
+    console.log('connected client');
+    client.spawn('bash', null , null, function (err, child) {
+        child.stdout.on('data', function (data) {
+            console.log(data);
+        });
+        child.exec('ls -alh');
+        child.exec('git status');
+        child.exec('git branch');
+        //child.exec('git commit -am "adding socproc"');
+        //child.exec('git push');
+    });
+});
+
+/*io.on('connection', function (socket) {
+ console.log('connected');
+ socket.emit('news', { hello: 'world' });
+ socket.on('my other event', function (data) {
+ console.log(data);
+ });
+ });*/
+/*
+ var wss = new ws.Server({
+ server: server
+ });
+
+ wss.on('connection', function (socket) {
+ console.log('connected');
+ });*/
